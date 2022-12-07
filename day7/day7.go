418df66691c5979ea888327981f2da4bf97dfe24
Ryan Ritz <ryan.ritz@redcanary.com>
2022-12-07 10:07:54 -0700

    Clean up

--- day7/day7.go
+++ day7/day7.go
@@ -4,12 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func findDirectories(part int) int {
-	readFile, err := os.Open("day7_input_ex.txt")
+type Directory struct {
+	name      string
+	parent    *Directory
+	fileSizes []int
+	subDirs   map[string]*Directory
+}
+
+var dirsLess100k = 0
+var dirsLess100kSize = 0
+var allDirs []int
+
+func findDirectories() int {
+	readFile, err := os.Open("day7_input.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -24,57 +36,73 @@ func findDirectories(part int) int {
 
 	readFile.Close()
 
-	// Object = ["dirName"]["parent", "dir/filenames..."]interface{}
-
-	type Directory struct {
-		name string
-		parent *Directory
-		fileSizes []int
-		subDirs map[string]*Directory
-	}
-
-	// filesystem := make(map[string]Directory)
-	var filesystem Directory
-	var currentDirectory Directory
+	var filesystem *Directory
+	var currentDirectory *Directory
 
 	for _, line := range fileLines {
-		if strings.HasPrefix(line,"$") {
-			fmt.Println("COMMAND: ", line)
+		if strings.HasPrefix(line, "$") {
 			if line[2:4] == "cd" {
 				cdDirectory := line[5:]
 				if cdDirectory == "/" {
 					dir := Directory{name: cdDirectory, parent: nil, subDirs: make(map[string]*Directory), fileSizes: []int{}}
-					filesystem = dir
-					currentDirectory = dir
+					filesystem = &dir
+					currentDirectory = &dir
 				} else if cdDirectory == ".." {
-					fmt.Println(".. CUR DIR: ", currentDirectory)
-					currentDirectory = *currentDirectory.parent
-					fmt.Println(".. PAR DIR: ", currentDirectory)
+					currentDirectory = currentDirectory.parent
 				} else {
-					fmt.Println("CURRENT DIRECTORY: ", currentDirectory)
-					currentDirectory = *currentDirectory.subDirs[cdDirectory]
+					currentDirectory = currentDirectory.subDirs[cdDirectory]
 				}
 			} else {
 				// ls command
 				continue
 			}
 		} else if strings.HasPrefix(line, "dir") {
-			fmt.Println("CURRENT DIRECTORY: ", currentDirectory)
-			fmt.Println("DIRECTORY: ", line)
-			newDir := Directory{name: line[4:], parent: &currentDirectory, subDirs: make(map[string]*Directory), fileSizes: []int{}}
+			newDir := Directory{name: line[4:], parent: currentDirectory, subDirs: make(map[string]*Directory), fileSizes: []int{}}
 			currentDirectory.subDirs[line[4:]] = &newDir
 		} else {
-			fmt.Println("FILE: ", line)
 			fileDetails := strings.Split(line, " ")
 			fileSize, _ := strconv.Atoi(fileDetails[0])
 			currentDirectory.fileSizes = append(currentDirectory.fileSizes, fileSize)
 		}
 	}
-	fmt.Println(filesystem)
-	return -1
+
+	totalSize := directorySize(*filesystem)
+
+	return totalSize
+}
+
+func directorySize(dir Directory) int {
+	dirSize := 0
+	for _, subDir := range dir.subDirs {
+		dirSize += directorySize(*subDir)
+	}
+
+	for _, fileSize := range dir.fileSizes {
+		dirSize += fileSize
+	}
+
+	if dirSize <= 100000 {
+		dirsLess100k++
+		dirsLess100kSize += dirSize
+	}
+
+	allDirs = append(allDirs, dirSize)
+
+	return dirSize
 }
 
 func main() {
-	fmt.Println("PART 1 INDEX: ", findDirectories(1))
-	// fmt.Println("PART 2 INDEX: ", findDirectories(2))
+	fileSystemSize := findDirectories()
+	fmt.Println("100K Dir Size Total: ", dirsLess100kSize)
+
+	remainingSpace := 70000000 - fileSystemSize
+	spaceNeeded := 30000000 - remainingSpace
+
+	sort.Ints(allDirs)
+	for _, sz := range allDirs {
+		if sz >= spaceNeeded {
+			fmt.Println("Smallest size of deleteable dir: ", sz)
+			break
+		}
+	}
 }
