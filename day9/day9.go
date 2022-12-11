dadb893e4fc3388cc11db7a6df1c3a687cdddd1d
ryan <ritzbitz01@gmail.com>
2022-12-11 11:39:31 -0700

    Use the right index

--- day9/day9.go
+++ day9/day9.go
@@ -15,8 +15,8 @@ type Position struct {
 }
 
 func findMarker(part int) int {
-	// readFile, err := os.Open("day9_input.txt")
-	readFile, err := os.Open("day9_input_ex2.txt")
+	readFile, err := os.Open("day9_input.txt")
+	// readFile, err := os.Open("day9_input_ex2.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -34,14 +34,10 @@ func findMarker(part int) int {
 	tailPosVisitedTotal := 0
 
 	// Track the positions tail has been
-	// var tailPositions []Position
 	tailPositions := make(map[string]bool)
 
-	// headPosition := Position{x: 0, y: 0}
-	// tailPosition := Position{x: 0, y: 0}
 	allTailPositions := []Position{{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}}
 
-	// fmt.Println("TAIL POS: ", tailPosition.x, " ", tailPosition.y)
 	tailPositions["0_0"] = true
 	tailPosVisitedTotal++
 	for _, line := range fileLines {
@@ -50,7 +46,6 @@ func findMarker(part int) int {
 		numSteps, _ := strconv.Atoi(vals[1])
 		switch direction {
 		case "U":
-			fmt.Println("MOVE UP: ", numSteps)
 			for i := 0; i < numSteps; i++ {
 				allTailPositions[0].y++
 				for j := 1; j < len(allTailPositions); j++ {
@@ -84,9 +79,8 @@ func findMarker(part int) int {
 						tailMoved = true
 					}
 					if tailMoved {
-						if i == 9 {
+						if j == 9 {
 							if !tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] {
-								fmt.Println("TAIL POS: ", allTailPositions[j].x, " ", allTailPositions[j].y)
 								tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] = true
 								tailPosVisitedTotal++
 							}
@@ -95,7 +89,6 @@ func findMarker(part int) int {
 				}
 			}
 		case "R":
-			fmt.Println("MOVE RIGHT: ", numSteps)
 			for i := 0; i < numSteps; i++ {
 				allTailPositions[0].x++
 				for j := 1; j < len(allTailPositions); j++ {
@@ -129,9 +122,8 @@ func findMarker(part int) int {
 						tailMoved = true
 					}
 					if tailMoved {
-						if i == 9 {
+						if j == 9 {
 							if !tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] {
-								fmt.Println("TAIL POS: ", allTailPositions[j].x, " ", allTailPositions[j].y)
 								tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] = true
 								tailPosVisitedTotal++
 							}
@@ -140,7 +132,6 @@ func findMarker(part int) int {
 				}
 			}
 		case "D":
-			fmt.Println("MOVE DOWN: ", numSteps)
 			for i := 0; i < numSteps; i++ {
 				allTailPositions[0].y--
 				for j := 1; j < len(allTailPositions); j++ {
@@ -174,9 +165,8 @@ func findMarker(part int) int {
 						tailMoved = true
 					}
 					if tailMoved {
-						if i == 9 {
+						if j == 9 {
 							if !tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] {
-								fmt.Println("TAIL POS: ", allTailPositions[j].x, " ", allTailPositions[j].y)
 								tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] = true
 								tailPosVisitedTotal++
 							}
@@ -185,7 +175,6 @@ func findMarker(part int) int {
 				}
 			}
 		case "L":
-			fmt.Println("MOVE LEFT: ", numSteps)
 			for i := 0; i < numSteps; i++ {
 				allTailPositions[0].x--
 				for j := 1; j < len(allTailPositions); j++ {
@@ -221,44 +210,19 @@ func findMarker(part int) int {
 					if tailMoved {
 						if j == 9 {
 							if !tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] {
-								fmt.Println("TAIL POS: ", allTailPositions[j].x, " ", allTailPositions[j].y)
 								tailPositions[fmt.Sprintf("%d_%d", allTailPositions[j].x, allTailPositions[j].y)] = true
 								tailPosVisitedTotal++
 							}
 						}
 					}
 				}
-				// fmt.Println("------- STEP ", i, " --------")
-				// fmt.Println("HEAD POSITION: ", allTailPositions[0].x, " ", allTailPositions[0].y)
-				// fmt.Println("SECOND POSITION: ", allTailPositions[1].x, " ", allTailPositions[1].y)
-				// fmt.Println("THIRD POSITION: ", allTailPositions[2].x, " ", allTailPositions[2].y)
-				// fmt.Println("FOURTH POSITION: ", allTailPositions[3].x, " ", allTailPositions[3].y)
-				// fmt.Println("FIFTH POSITION: ", allTailPositions[4].x, " ", allTailPositions[4].y)
-				// fmt.Println("SIXTH POSITION: ", allTailPositions[5].x, " ", allTailPositions[5].y)
-				// fmt.Println("SEVENTH POSITION: ", allTailPositions[6].x, " ", allTailPositions[6].y)
-				// fmt.Println("EIGHTH POSITION: ", allTailPositions[7].x, " ", allTailPositions[7].y)
-				// fmt.Println("NINTH POSITION: ", allTailPositions[8].x, " ", allTailPositions[8].y)
-				// fmt.Println("TAIL POSITION: ", allTailPositions[9].x, " ", allTailPositions[9].y)
 			}
 		default:
 			fmt.Println("UNKNOWN DIRECTION: ", direction)
 		}
-		fmt.Println("HEAD POSITION: ", allTailPositions[0].x, " ", allTailPositions[0].y)
-		fmt.Println("SECOND POSITION: ", allTailPositions[1].x, " ", allTailPositions[1].y)
-		fmt.Println("THIRD POSITION: ", allTailPositions[2].x, " ", allTailPositions[2].y)
-		fmt.Println("FOURTH POSITION: ", allTailPositions[3].x, " ", allTailPositions[3].y)
-		fmt.Println("FIFTH POSITION: ", allTailPositions[4].x, " ", allTailPositions[4].y)
-		fmt.Println("SIXTH POSITION: ", allTailPositions[5].x, " ", allTailPositions[5].y)
-		fmt.Println("SEVENTH POSITION: ", allTailPositions[6].x, " ", allTailPositions[6].y)
-		fmt.Println("EIGHTH POSITION: ", allTailPositions[7].x, " ", allTailPositions[7].y)
-		fmt.Println("NINTH POSITION: ", allTailPositions[8].x, " ", allTailPositions[8].y)
-		fmt.Println("TAIL POSITION: ", allTailPositions[9].x, " ", allTailPositions[9].y)
 	}
 
-	fmt.Println("TAIL POSITIONS: ", tailPositions)
-
 	return tailPosVisitedTotal
-
 }
 
 func main() {
